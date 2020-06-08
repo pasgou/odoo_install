843f0baa82c514e77533055d5efc0244ec5d0e90
Pascal GOUHIER <pascal.gouhier@copagex.fr>
2020-06-08 19:13:29 +0200

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -152,21 +152,21 @@ sudo chown -R $OE_USER:$OE_USER $OE_HOME
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
 sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/OCA/OCB.git $OE_HOME_EXT/"
-sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/it-projects-llc/saas-addons.git $OE_IMPORTED_ADDONS/saas-addons/"
-sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/itpp-labs/access-addons.git $OE_IMPORTED_ADDONS/access-addons/"
-sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/itpp-labs/misc-addons.git $OE_IMPORTED_ADDONS/misc-addons/"
-sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/OCA/web.git $OE_IMPORTED_ADDONS/web/"
-sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/OCA/queue.git $OE_IMPORTED_ADDONS/queue/"
+#sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/it-projects-llc/saas-addons.git $OE_IMPORTED_ADDONS/saas-addons/"
+#sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/itpp-labs/access-addons.git $OE_IMPORTED_ADDONS/access-addons/"
+#sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/itpp-labs/misc-addons.git $OE_IMPORTED_ADDONS/misc-addons/"
+#sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/OCA/web.git $OE_IMPORTED_ADDONS/web/"
+#sudo su odoo -c "git clone --branch $OE_VERSION https://github.com/OCA/queue.git $OE_IMPORTED_ADDONS/queue/"
 sudo pip3 install wheel
 sudo pip3 install -r $OE_HOME_EXT/requirements.txt
-sudo pip3 install -r $OE_IMPORTED_ADDONS/saas-addons/requirements.txt
-sudo pip3 install -r $OE_IMPORTED_ADDONS/misc-addons/requirements.txt
-sudo pip3 install -r $OE_IMPORTED_ADDONS/web/requirements.txt
-sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/saas-addons/* $OE_IMPORTED_ADDONS"
-sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/access-addons/* $OE_IMPORTED_ADDONS"
-sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/misc-addons/* $OE_IMPORTED_ADDONS"
-sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/web/* $OE_IMPORTED_ADDONS"
-sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/queue/* $OE_IMPORTED_ADDONS"
+#sudo pip3 install -r $OE_IMPORTED_ADDONS/saas-addons/requirements.txt
+#sudo pip3 install -r $OE_IMPORTED_ADDONS/misc-addons/requirements.txt
+#sudo pip3 install -r $OE_IMPORTED_ADDONS/web/requirements.txt
+#sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/saas-addons/* $OE_IMPORTED_ADDONS"
+#sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/access-addons/* $OE_IMPORTED_ADDONS"
+#sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/misc-addons/* $OE_IMPORTED_ADDONS"
+#sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/web/* $OE_IMPORTED_ADDONS"
+#sudo su odoo -c "mv -f $OE_IMPORTED_ADDONS/queue/* $OE_IMPORTED_ADDONS"
 sudo pip3 install python-barcode request
 
 echo -e "* Create server config file"
@@ -192,10 +192,10 @@ sudo su root -c "echo 'auto_reload = True' >> $OE_CONFDIR/$OE_CONFIG.conf"
 sudo su root -c "echo 'addons_path=$OE_HOME_EXT/addons,$OE_IMPORTED_ADDONS,$OE_CUSTOM_ADDONS,$OE_PRIVATE_ADDONS' >> $OE_CONFDIR/$OE_CONFIG.conf"
 sudo su root -c "echo 'syslog = True' >> $OE_CONFDIR/$OE_CONFIG.conf"
 sudo su root -c "echo 'data_dir = $OE_FILESTORE' >> $OE_CONFDIR/$OE_CONFIG.conf"
-sudo su root -c "echo 'workers = 2' >> $OE_CONFDIR/$OE_CONFIG.conf"
+sudo su root -c "echo 'workers = 3' >> $OE_CONFDIR/$OE_CONFIG.conf"
 sudo su root -c "echo 'server_wide_modules = web,queue_job,saas' >> $OE_CONFDIR/$OE_CONFIG.conf"
-sudo su root -c "echo '[queue_job]' >> $OE_CONFDIR/$OE_CONFIG.conf"
-sudo su root -c "echo 'channels = root:2' >> $OE_CONFDIR/$OE_CONFIG.conf"
+#sudo su root -c "echo '[queue_job]' >> $OE_CONFDIR/$OE_CONFIG.conf"
+#sudo su root -c "echo 'channels = root:2' >> $OE_CONFDIR/$OE_CONFIG.conf"
 
 sudo cp $OE_CONFDIR/$OE_CONFIG.conf $OE_CONFIG.conf
 
@@ -235,5 +235,5 @@ sudo systemctl daemon-reload
 sudo systemctl start odoo12
 sudo systemctl enable odoo12
 
-echo "Done! The ODOO server can be started with: service $OE_CONFIG start"
-echo "You can create your first Database with this SuperAdmin Password : $OE_SUPERADMIN (copy/paste it)."
+#echo "Done! The ODOO server can be started with: service $OE_CONFIG start"
+#echo "You can create your first Database with this SuperAdmin Password : $OE_SUPERADMIN (copy/paste it)."
